fa378bb984ff7763bff2fb9e312bd556ab707ce3
Sinaf19 <quentinsurdez@gmail.com>
2025-03-11 13:57:56 +0100

    feat: add comment under post

--- blog_example/blog/views.py
+++ blog_example/blog/views.py
@@ -1,8 +1,9 @@
 from django.core.mail import send_mail
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, get_object_or_404
+from django.views.decorators.http import require_POST
 from django.views.generic import ListView
-from .forms import EmailPostForm
+from .forms import EmailPostForm, CommentForm
 from .models import Post
 
 
@@ -82,8 +83,36 @@ def post_detail(request, year, month, day, post):
         publish__day=day,
         status=Post.Status.PUBLISHED,
     )
+    # List of active comment for this post
+    comments = post.comments.filter(active=True)
+    # Form for users to comment
+    form = CommentForm()
     return render(
         request,
         "blog/post/detail.html",
-        {"post": post},
+        {"post": post, "comments": comments, "form": form},
+    )
+
+
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    comment = None
+    # A comment was posted
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        # Create a comment object without saving it
+        comment = form.save(commit=False)
+        # Assign the post to the comment
+        comment.post = post
+        # Save the comment to the database
+        comment.save()
+    return render(
+        request,
+        "blog/post/comment.html",
+        {
+            "post": post,
+            "form": form,
+            "comment": comment,
+        },
     )
