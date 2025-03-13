cede4fd59486a6e150288a374d15bbab48cffde7
Sinaf19 <quentinsurdez@gmail.com>
2025-03-13 13:47:16 +0100

    feat: add trigram similarity to search enfine

--- blog_example/blog/views.py
+++ blog_example/blog/views.py
@@ -2,7 +2,7 @@ from django.core.mail import send_mail
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.db.models import Count
 from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
-from django.db.models.expressions import result
+from django.contrib.postgres.search import TrigramSimilarity
 from django.shortcuts import render, get_object_or_404
 from django.views.decorators.http import require_POST
 from django.views.generic import ListView
@@ -146,16 +146,15 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data["query"]
-            search_vector = SearchVector("title", weight="A") + SearchVector(
-                "body", weight="B"
-            )
-            search_query = SearchQuery(query)
+            # search_vector = SearchVector("title", weight="A") + SearchVector(
+            #     "body", weight="B"
+            # )
+            # search_query = SearchQuery(query)
             results = (
                 Post.published.annotate(
-                    search=search_vector,
-                    rank=SearchRank(search_vector, search_query),
+                    similarity=TrigramSimilarity("title", query),
                 )
-                .filter(rank__gte=0.3)
+                .filter(similarity__gt=0.1)
                 .order_by("-rank")
             )
     return render(
