68ff6ee3830e066c2d36b3064224e917822b897f
Sinaf19 <quentinsurdez@gmail.com>
2025-03-13 15:00:00 +0100

    feat: add login, logout views and logic

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -1,8 +1,20 @@
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 
 from account.forms import LoginForm
+
+
+@login_required
+def dashboard(request):
+    return render(
+        request,
+        "account/dashboard.html",
+        {
+            "section": "dashboard",
+        },
+    )
 
 
 def user_login(request):
