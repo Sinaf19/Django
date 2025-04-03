294c8f17efd16166df85f8d7e8f692198fc78d98
Sinaf19 <quentinsurdez@gmail.com>
2025-04-03 08:45:23 +0200

    feat: add render of all types of items

--- educa/courses/models.py
+++ educa/courses/models.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import User
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 from django.db import models
+from django.template.loader import render_to_string
 
 from courses.fields import OrderField
 
@@ -11,7 +12,7 @@ class Subject(models.Model):
     slug = models.SlugField(max_length=200, unique=True)
 
     class Meta:
-        ordering = ['title']
+        ordering = ["title"]
 
     def __str__(self):
         return self.title
@@ -19,38 +20,26 @@ class Subject(models.Model):
 
 class Course(models.Model):
     owner = models.ForeignKey(
-        User,
-        related_name="courses_created",
-        on_delete=models.CASCADE
+        User, related_name="courses_created", on_delete=models.CASCADE
     )
     subject = models.ForeignKey(
-        Subject,
-        related_name="courses",
-        on_delete=models.CASCADE
+        Subject, related_name="courses", on_delete=models.CASCADE
     )
-    students = models.ManyToManyField(
-        User,
-        related_name="courses_joined",
-        blank=True
-    )
+    students = models.ManyToManyField(User, related_name="courses_joined", blank=True)
     title = models.CharField(max_length=200)
     slug = models.SlugField(max_length=200, unique=True)
     overview = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        ordering = ['-created']
+        ordering = ["-created"]
 
     def __str__(self):
         return self.title
 
 
 class Module(models.Model):
-    course = models.ForeignKey(
-        Course,
-        related_name="modules",
-        on_delete=models.CASCADE
-    )
+    course = models.ForeignKey(Course, related_name="modules", on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     description = models.TextField(blank=True)
     order = OrderField(blank=True, for_fields=["course"])
@@ -64,21 +53,17 @@ class Module(models.Model):
 
 class Content(models.Model):
     module = models.ForeignKey(
-        Module,
-        related_name='contents',
-        on_delete=models.CASCADE
+        Module, related_name="contents", on_delete=models.CASCADE
     )
     content_type = models.ForeignKey(
         ContentType,
         on_delete=models.CASCADE,
-        limit_choices_to={
-            "model__in": ("text", "image", "video", "file")
-        }
+        limit_choices_to={"model__in": ("text", "image", "video", "file")},
     )
     # This is the sign of a badly developed relational schema
     # This shall not exist in my projects no no no
     object_id = models.PositiveIntegerField()
-    item = GenericForeignKey('content_type', 'object_id')
+    item = GenericForeignKey("content_type", "object_id")
     order = OrderField(blank=True, for_fields=["module"])
 
     class Meta:
@@ -86,10 +71,9 @@ class Content(models.Model):
 
 
 class ItemBase(models.Model):
-    owner = models.ForeignKey(User,
-                              related_name='%(class)s_related',
-                              on_delete=models.CASCADE
-                              )
+    owner = models.ForeignKey(
+        User, related_name="%(class)s_related", on_delete=models.CASCADE
+    )
     title = models.CharField(max_length=250)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
@@ -100,17 +84,22 @@ class ItemBase(models.Model):
     def __str__(self):
         return self.title
 
+    def render(self):
+        return render_to_string(
+            f"courses/content/{self._meta.model_name}.html", {"item": self}
+        )
+
 
 class Text(ItemBase):
     content = models.TextField()
 
 
 class File(ItemBase):
-    file = models.FileField(upload_to='files')
+    file = models.FileField(upload_to="files")
 
 
 class Image(ItemBase):
-    file = models.FileField(upload_to='images')
+    file = models.FileField(upload_to="images")
 
 
 class Video(ItemBase):
