d3eae6ed05b96db834123ca40ec1acad569a27d0
Sinaf19 <quentinsurdez@gmail.com>
2025-04-03 14:37:10 +0200

    feat: add auth to consume example

--- educa/api_examples/enroll_all.py
+++ educa/api_examples/enroll_all.py
@@ -1,5 +1,7 @@
 import requests
 
+username = "student"
+password = "1234toto"
 base_url = "http://127.0.0.1:8000/api/"
 url = f"{base_url}courses/"
 available_courses = []
@@ -12,3 +14,11 @@ while url is not None:
     courses = response["results"]
     available_courses += [course["title"] for course in courses]
 print(f"Available courses: {", ".join(available_courses)}")
+for course in courses:
+    course_id = course["id"]
+    course_title = course["title"]
+    r = requests.post(
+        f"{base_url}courses/{course_id}/enroll/", auth=(username, password)
+    )
+    if r.status_code == 200:
+        print(f"Successfully enrolled in {course_title}")
