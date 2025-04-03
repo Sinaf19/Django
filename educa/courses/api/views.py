3255dc377fcf0fbef15fd88959c9954001c22491
Sinaf19 <quentinsurdez@gmail.com>
2025-04-03 13:40:37 +0200

    feat: add auth + permissions to post route

--- educa/courses/api/views.py
+++ educa/courses/api/views.py
@@ -2,6 +2,9 @@ from django.db.models import Count
 from django.shortcuts import get_object_or_404
 from rest_framework import generics
 from rest_framework import viewsets
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.decorators import action, authentication_classes, permission_classes
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -24,6 +27,18 @@ class SubjectDetailView(generics.RetrieveAPIView):
 class CourseViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Course.objects.prefetch_related("modules")
     serializer_class = CourseSerializer
+
+    @action(
+        detail=True,
+        methods=["post"],
+        authentication_classes=[BasicAuthentication],
+        permission_classes=[IsAuthenticated],
+    )
+    def enroll(self, request, *args, **kwargs):
+        course = self.get_object()
+        course.students.add(request.user)
+        return Response({"enrolled": True})
+
     pagination_class = StandardPagination
 
 
@@ -34,6 +49,9 @@ class SubjectViewSet(viewsets.ReadOnlyModelViewSet):
 
 
 class CourseEnrollView(APIView):
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+
     def post(self, request, pk, format=None):
         course = get_object_or_404(Course, pk=pk)
         course.students.add(request.user)
