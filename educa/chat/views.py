c66b3e2fa73d2ee0dfe3e569e6fd3b7bb2044ca5
Sinaf19 <quentinsurdez@gmail.com>
2025-04-03 15:03:51 +0200

    feat: add function view for chat room + url path

--- educa/chat/views.py
+++ educa/chat/views.py
@@ -1,1 +1,16 @@
-# Create your views here.
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseForbidden
+from django.shortcuts import render
+
+from courses.models import Course
+
+
+@login_required
+def course_chat_room(request, course_id):
+    try:
+        # retrieve course with given id joined by the current user
+        course = request.user.courses__joined.get(id=course_id)
+    except Course.DoesNotExist:
+        # user is not a student of the course or the course does not exist
+        return HttpResponseForbidden()
+    return render(request, "chat/room.html", {"course": course})
